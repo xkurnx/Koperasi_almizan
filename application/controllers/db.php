a99581ea69aed83eb19f011eccc6cd8aa58c1afc
xkurnx <xkurnx@gmail.com>
2014-05-25 11:41:10 +0700

    made-restore-file-work

--- application/controllers/db.php
+++ application/controllers/db.php
@@ -9,7 +9,7 @@ class Db extends CI_Controller {
 	{
 		parent::__construct();
 		// load library
-		$this->load->library(array('table','form_validation','session','kur_functions'));
+		$this->load->library(array('table','form_validation','session','kur_functions','Unzip'));
 		$this->limit = 20;
 		
 		// load helper
@@ -35,9 +35,65 @@ class Db extends CI_Controller {
 		$tanggal=date('Ymd');
 		$namaFile= 'db_koperasi_' . $tanggal . '.sql.zip';
 		$this->load->dbutil();
-		$backup=& $this->dbutil->backup();
+		
+		$prefs = array(
+                'format'      => 'zip',
+				'filename'    => 'dbAppKopAlmizan.sql',    // File name - NEEDED ONLY WITH ZIP FILES
+				'add_drop'    => TRUE,              // Whether to add DROP TABLE statements to backup file
+                'add_insert'  => TRUE,              // Whether to add INSERT data to backup file
+                'newline'     => "\n"               // Newline character used in backup file
+              );
+
+		$backup = $this->dbutil->backup($prefs); 
 		force_download($namaFile, $backup);
 	}
+	
+	function restore(){
+		$data['error'] = '';
+		echo $this->load->view('upload_sql', $data,true);
+	
+	}
+	
+	function do_restore(){
+		$name=time();
+		$config['upload_path'] = './uploads/';
+		$config['allowed_types'] = 'zip|rar';
+		if ( !$this->input->post('pwd') != 'almizan2014' )
+		{
+			exit('Access Denied');
+		}
+		
+		$this->load->library('upload', $config);
+		
+		if ( ! $this->upload->do_upload())
+		{
+			$error = array('error' => $this->upload->display_errors());
+
+			$this->load->view('upload_sql', $error);
+		}
+		else
+		{
+			$data = array('upload_data' => $this->upload->data());
+			// Optional: Only take out these files, anything else is ignored
+			$this->unzip->allow(array('sql', 'js', 'png', 'gif', 'jpeg', 'jpg', 'tpl', 'html', 'swf'));
+			#echo $data['upload_data']['full_path'];
+			$extracted = $this->unzip->extract($data['upload_data']['full_path']) ;
+			
+			$this->load->helper('file');
+			$query = utf8_decode (read_file($extracted [0] ));
+			$sqls = explode(';', $query);
+				array_pop($sqls);
+				 
+				foreach($sqls as $statement){
+					$this->db->query("SET FOREIGN_KEY_CHECKS = 0");
+					$this->db->query($statement);
+					$this->db->query("SET FOREIGN_KEY_CHECKS = 1");
+				}
+				echo "done";
+		}
+		
+	}
+	
 	
 	
 }
